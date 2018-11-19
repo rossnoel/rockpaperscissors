247a65416749f0b9ea0e8f703ba33e8663bff7ee
Rasheeda Noel <rasheedanoel@gmail.com>
2018-11-19 23:13:52 +0000

    rock paper scissors

--- js/script.js
+++ js/script.js
@@ -8,12 +8,32 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-var userChoice;{
+var userChoice;
+$("button").click(function(){
+var input = $("input").val();
+$("userChoice").text(input);
+var randomNumber=Math.random();
+if(input ===0.0-0.33) {
+        $("#text").text("scissors"); 
+    } else if( input <.66  && fortune> ) $("#text").text("sorry");
+});
     
-}
-var computerChoice;{
+}); 
+
+var computerChoice;$("button").click(function(){
+var input = $("input").val() + "!";
+
+$("computerChoice").text(input);
+
+    if(input===scissors ) {
+        $("#text").text(""); 
+    } else if( < && > 0.6) $("#text").text("");
+
+});
     
-}
 var winner;
-var randomNumber =0;
-    
+var randomNumber =Math.random();
+    if(> ) {
+        $("#text").text(""); 
+    } else if( < && > 0.6) $("#text").text("");
+
